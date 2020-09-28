f764406e4e97995b0884683acd207034053db184
melaine229 <melainecarmellebokpe@gmail.com>
2020-09-28 10:28:49 +0100

    mon essaie sur le javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+let name = "toto";
+console.log(name);
+
+let numbers = [0,1,2,3,4,5,6,7,8,9];
+let cars = {
+    nom: 'mercedes, Volkswagen, BMW'
+};
+let success = true;
+let bigArray= [numbers, cars.name ,success];
+console.log(bigArray); 
+
+function carré(number) {
+    
+	var carré = Math. sqrt(number) ;
+    return carré;
+  
+}
+console.log("carré");
+ 
