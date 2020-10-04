f802fba90a2eb2774bbd23bcf7a2cb556939c298
melaine229 <melainecarmellebokpe@gmail.com>
2020-10-04 08:20:26 +0100

    correction

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ console.log(name);
 
 let numbers = [0,1,2,3,4,5,6,7,8,9];
 let cars = {
-    nom: 'mercedes, Volkswagen, BMW'
+    nom: ['mercedes, Volkswagen, BMW']
 };
 let success = true;
-let bigArray= [numbers, cars.name ,success];
+let bigArray= [numbers, cars ,success];
 console.log(bigArray); 
 
 function carré(number) {
     
-	var carré = Math. sqrt(number) ;
+	var carré = Math. pow(number,2) ;
     return carré;
   
 }
-console.log("carré");
+console.log(carré(5));
  
